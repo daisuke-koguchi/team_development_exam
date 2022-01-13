ef1468f1c1e2388924e36a6a276691bb808417f7
vagrant <vagrant@vagrant.vm>
2022-01-13 04:00:46 +0000

    update Rubocopでコード修正を実施

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,7 +15,7 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
@@ -23,8 +23,8 @@ class AgendasController < ApplicationController
 
   def destroy
     @send_email_member = @agenda.team.users
-    @agenda.destroy 
-     #ここにメール送信アクションを追加
+    @agenda.destroy
+    # ここにメール送信アクションを追加
     @send_email_member.each do |member|
       AgendaMailer.agenda_mail(@agenda.title, member.email).deliver
     end
--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,7 @@
 class AgendaMailer < ApplicationMailer
   def agenda_mail(title, email)
-    @title = title 
-    @email = email 
+    @title = title
+    @email = email
     mail to: @email, subject: '[DIVE INTO POST] アジェンダ削除のお知らせ'
   end
 end
